3bf41f3de1dd42d08be474e3631a8059b1768996
cngumbi <cngumbi35@gmail.com>
2020-05-26 14:40:03 +0300

    new commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#creating the setup
+from setuptools import setup
+
+#the arguements are
+setup(
+    name     = "SOFTCRAZE"
+    version  = "0.0.1"
+    author   = "cngumbi"
+    author_email = "cngumbi35@gmail.com"
+    description = "The main softcraze website"
+    license  = ""
+    keywords = "flask, softcraze"
+    url = ""
+    packages = ""
+    long_description = ""
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Aproved :: ",
+    ],
+)
