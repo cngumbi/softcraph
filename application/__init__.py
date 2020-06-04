a0ff9d0401ef7ff740a4c76d201b2d9b639f9034
cngumbi <cngumbi35@gmail.com>
2020-06-04 17:41:51 +0300

    new change

--- application/__init__.py
+++ application/__init__.py
@@ -1,18 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from application import db
-#from application.users.views import users
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 
 #configer the sql database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../softcraze.db'
 db = SQLAlchemy(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+flask_bcrypt = Bcrypt()
+
+#local modules
+from application.users import models as user_models
+from application.users.views import users
 #app.register_blueprint(users, url_prefix='/users')
 
-#local packages
-#import application.models
-def contact():
+#loading a user from the database
+@login_manager.user_loader
+def load_user(user_id):
+    return application.user_models.query.get(int(user_id))
+"""def contact():
     return "my contacts"
 
 #create route to handle login
@@ -24,6 +36,7 @@ def login():
     else:
         #display login form
         pass
+    """
 """
 if __name__ == "__main__":
     app.run()
