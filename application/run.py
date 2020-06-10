e544097b853a6e31cb34a3a8b80114662fe25a80
cngumbi <cngumbi35@gmail.com>
2020-06-10 20:18:07 +0300

    new changes

---
+++ application/run.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/softcraph")
+def softcraph():
+    return "home page"
+
+
+@app.route("/about")
+def about():
+    return "about page"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
