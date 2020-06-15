87ad1cc2ed6cafc430c076661d2aa5943a394bf0
cngumbi <cngumbi35@gmail.com>
2020-06-15 07:06:38 +0300

    new changes

--- application/models.py
+++ application/models.py
@@ -1,16 +1,5 @@
-from datetime import datetime 
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, DateTime
-from addUser import AddUserForm, LoginForm
-
-app = Flask(__name__)
-#configer the secrect key
-app.config['SECRET_KEY'] = "&\xfb?\xfbL\xd7\xc0z\x19ewF\xdd\xe6\xce(M\xbc\x15,"
-#set the configaration of the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///softcraze.db'
-#create a database instance
-db = SQLAlchemy(app)
+from datetime import datetime
+from app import db
 
 
 #create a class for User
@@ -51,40 +40,3 @@ class Project(db.Model):
 
     def __repr__(self):
         return f"Project('{self.title}', '{self.created_on}')"
-
-
-
-
-@app.route("/")
-@app.route("/home")
-def softcraph():
-    return render_template('home.html')
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='about')
-
-@app.route("/adduser",methods=['GET', 'POST'])
-def adduser():
-    form = AddUserForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success') 
-        return redirect(url_for('adduser'))
-    return render_template('adduser.html', title='Add User', form=form)
-
-
-@app.route("/login",methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('softcraph'))
-        else:
-            flash('Login Unsuccessful. PLease check username and password', 'danger') 
-    return render_template('Login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
