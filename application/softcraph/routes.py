5a94f6a1c57a65a1ed2c1ef696271316bcaee61b
cngumbi <cngumbi35@gmail.com>
2020-06-18 14:16:07 +0300

    new changes

--- application/softcraph/routes.py
+++ application/softcraph/routes.py
@@ -21,8 +21,8 @@ def softcraph():
     #client images
     client_1 =os.path.join(app.config['UPLOAD_FOLDER'], 'jwc.png')
     client_2 =os.path.join(app.config['UPLOAD_FOLDER'], 'dyt1.png')
-    client_3 =os.path.join(app.config['UPLOAD_FOLDER'], 'client-1.png')
-    client_4 =os.path.join(app.config['UPLOAD_FOLDER'], 'client-2.png')
+    client_3 =os.path.join(app.config['UPLOAD_FOLDER'], 'SOFTCRAZE.png')
+    client_4 =os.path.join(app.config['UPLOAD_FOLDER'], 'talanta.png')
     client_5 =os.path.join(app.config['UPLOAD_FOLDER'], 'client-3.png')
     client_6 =os.path.join(app.config['UPLOAD_FOLDER'], 'client-4.png')
     #quotes png
@@ -31,6 +31,7 @@ def softcraph():
     #testimony png
     edith =url_for('static', filename='img/edith.png')
     dymetrian_tech =url_for('static', filename='img/dyt.png')
+    dantey_image =url_for('static', filename='img/dantey.png')
     #user_profile image
     image_file = url_for('static', filename='img/default.jpg')
     #about image
@@ -39,7 +40,8 @@ def softcraph():
                                 image_files=image_file, image_file1=image_files, portfolio_1=portF_1, portfolio_2=portF_2,
                                 portfolio_3=portF_3, portfolio_4=portF_4, favicon=favics, apple=apple_touch,
                                 quoteleft=quote_left, quoteright=quote_right, client2=client_2, client1=client_1, client3=client_3,
-                                client4=client_4, client5=client_5, client6=client_6, testimony=edith, dymetrian=dymetrian_tech)
+                                client4=client_4, client5=client_5, client6=client_6, testimony=edith, dymetrian=dymetrian_tech,
+                                dantey=dantey_image)
 
 
 @app.route("/staff")
